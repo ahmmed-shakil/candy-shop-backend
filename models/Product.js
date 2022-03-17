b324b9a01023526a5a92324be31ef9e2b053eb81
ahmmed-shakile <shakilahmmed252@gmail.com>
2022-03-18 05:07:14 +0600

    backend

---
+++ models/Product.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        categories: {
+            type: Array
+        },
+        desc1: {
+            type: String,
+            required: true
+        },
+        desc2: {
+            type: String,
+            required: true
+        },
+        desc3: {
+            type: String,
+            required: true
+        },
+        img1: {
+            type: String,
+            required: true
+        },
+        img2: {
+            type: String,
+            required: true
+        },
+        img3: {
+            type: String,
+            required: true
+        },
+        stock: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true
+        }
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("Product", ProductSchema);
