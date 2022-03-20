4dc8f2cceaa3406fba5d3df4c1f007cad48916a8
ahmmed-shakile <shakilahmmed252@gmail.com>
2022-03-20 09:28:10 +0600

    backend complete2

---
+++ routes/category.js
@@ -0,0 +1,15 @@
+const Product = require("../models/Product");
+
+
+
+const router = require("express").Router();
+
+
+router.get("/:category", async (req, res) => {
+    try {
+        const products = await Product.find({ category: req.params.category })
+        res.status(200).json(products)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
